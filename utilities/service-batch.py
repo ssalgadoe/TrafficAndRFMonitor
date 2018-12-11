07687de10a4e57e31dc0130de4ef4c0e6349019c
ssalgadoe <sajeeva.salgadoe@gmail.com>
2018-12-11 17:47:39 -0500

    Update service-batch.py

--- utilities/service-batch.py
+++ utilities/service-batch.py
@@ -4,10 +4,10 @@ import os
 path = "c:\www\monitor"
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-result = sock.connect_ex(('192.168.100.191',8081))
+result = sock.connect_ex(('.....',8081))
 if result == 0:
    print ("Port is open")
 else:
     os.chdir(path)
-    os.system("python manage.py runserver 192.168.100.191:8081")
+    os.system("python manage.py runserver .....:8081")
     print ("Port is not open")
